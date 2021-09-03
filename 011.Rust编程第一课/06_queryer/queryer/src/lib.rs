9a3165c5697da22eecf3ff300160995f014d3e32
Custer <custertian@163.com>
2021-09-03 21:51:47 +0800

    06_queryer

---
+++ 011.Rust编程第一课/06_queryer/queryer/src/lib.rs
@@ -0,0 +1,90 @@
+use anyhow::{anyhow, Result};
+use polars::prelude::*;
+use sqlparser::parser::Parser;
+use std::convert::TryInto;
+use std::ops::{Deref, DerefMut};
+use tracing::info;
+use convert::Sql;
+use loader::detect_content;
+use fetcher::retrieve_data;
+
+mod dialect;
+mod convert;
+mod fetcher;
+mod loader;
+
+pub use dialect::example_sql;
+pub use dialect::MyDialect;
+
+#[derive(Debug)]
+pub struct DataSet(DataFrame);
+
+/// 让 DataSet 用起来和 DataFrame 一致
+impl Deref for DataSet {
+    type Target = DataFrame;
+
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
+}
+
+/// 让 DataSet 用起来和 DataFrame 一致
+impl DerefMut for DataSet {
+    fn deref_mut(&mut self) -> &mut Self::Target {
+        &mut self.0
+    }
+}
+
+impl DataSet {
+    /// 从 DataSet 转换成 csv
+    pub fn to_csv(&self) -> Result<String> {
+        let mut buf = Vec::new();
+        let writer = CsvWriter::new(&mut buf);
+        writer.finish(self)?;
+        Ok(String::from_utf8(buf)?)
+    }
+}
+
+/// 从 from 中获取怇，从 where 中过滤，最后选取需要返回的列
+pub async fn query<T: AsRef<str>>(sql: T) -> Result<DataSet> {
+    let ast = Parser::parse_sql(&MyDialect::default(), sql.as_ref())?;
+
+    if ast.len() != 1 {
+        return Err(anyhow!("Only support single sql at the moment"));
+    }
+
+    let sql = &ast[0];
+
+    // 整个 SQL AST 转换成定义的 Sql 结构的细节都埋藏在 try_into() 中
+    // 我们只需关注数据结构的使用，怎么转换可以之后需要的时候才关注，这是
+    // 关注点分离，使我们控制软件复杂度的法宝
+    let Sql {
+        source,
+        condition,
+        selection,
+        offset,
+        order_by,
+        limit
+    } = sql.try_into()?;
+
+    info!("retrieving data from source: {}", source);
+
+    // 从 source 读入一个 DataSet
+    // detect_content，根据内容返回 DataSet
+    let ds = detect_content(retrieve_data(source).await?).load()?;
+
+    let mut filtered = match condition {
+        Some(expr) => ds.0.lazy().filter(expr),
+        None => ds.0.lazy(),
+    };
+
+    filtered = order_by
+        .into_iter()
+        .fold(filtered, |acc, (col, desc)| acc.sort(&col, desc));
+
+    if offset.is_some() || limit.is_some() {
+        filtered = filtered.slice(offset.unwrap_or(0), limit.unwrap_or(usize::MAX));
+    }
+
+    Ok(DataSet(filtered.select(selection).collect()?))
+}
