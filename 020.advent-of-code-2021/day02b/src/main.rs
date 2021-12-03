b60151f52de54b9ed12a05c550830ad00ba5d265
Custer <custertian@163.com>
2021-12-03 15:38:04 +0800

    AdventOfCode2021day02

---
+++ 020.advent-of-code-2021/day02b/src/main.rs
@@ -0,0 +1,29 @@
+use std::{fs::File, io::Read};
+
+fn main() {
+    let mut file = File::open("./day02b/input.txt").unwrap();
+    let mut contents = String::new();
+    file.read_to_string(&mut contents).unwrap();
+    let mut x = 0;
+    let mut y = 0;
+    let mut depth = 0;
+    for line in contents.lines() {
+        let mut param = line.split_whitespace();
+        match(param.next(), param.next()) {
+            (Some(order), Some(value)) => {
+                if order == "forward" {
+                    x += value.parse::<i32>().unwrap();
+                    depth += y * value.parse::<i32>().unwrap();
+                } else if order == "down" {
+                    y += value.parse::<i32>().unwrap();
+                } else if order == "up" {
+                    y -= value.parse::<i32>().unwrap();
+                }
+            },
+            _ => {
+                println!("Error parsing input");
+            }
+        }
+    }
+    println!("{}", x * depth);    
+}
