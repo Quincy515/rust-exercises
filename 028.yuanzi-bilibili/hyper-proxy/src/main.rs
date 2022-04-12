b0fb5681cf91b6313380816467f1b42c67e93b1b
CusterFun <custer@email.cn>
2022-04-12 13:29:03 +0800

    hyper proxy

---
+++ 028.yuanzi-bilibili/hyper-proxy/src/main.rs
@@ -0,0 +1,47 @@
+use anyhow::*;
+use std::sync::Arc;
+use std::net::SocketAddr;
+use hyper::{Body, Request, Server, Client};
+use hyper::service::{make_service_fn, service_fn};
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    let https = hyper_rustls::HttpsConnector::with_native_roots();
+    let client = Client::builder().build::<_, hyper::Body>(https);
+    let client = Arc::new(client);
+
+    let addr = SocketAddr::from(([0, 0, 0, 0], 7000));
+    let make_svc = make_service_fn(move |_conn| {
+        let client = Arc::clone(&client);
+        async move {
+            Ok(service_fn(
+                move |mut req| {
+                    let client = Arc::clone(&client);
+                    async move {
+                        println!("proxy: {}", req.uri().path());
+                        proxy_crate(&mut req)?;
+                        client.request(req).await.context("proxy request")
+                    }
+                }
+            ))
+        }
+    });
+
+    let _server = Server::bind(&addr).serve(make_svc).await.context("Run server");
+
+
+    Ok(())
+}
+
+fn proxy_crate(req: &mut Request<Body>) -> Result<()> {
+    for key in &["content-length", "accept-encoding", "content-encoding", "transfer-encoding"] {
+        req.headers_mut().remove(*key);
+    }
+    let uri = req.uri();
+    let uri_string = match uri.query() {
+        Some(query_item) => format!("https://crates.io{}?{}", uri.path(), query_item),
+        None => format!("https://crates.io{}", uri.path()),
+    };
+    *req.uri_mut() = uri_string.parse().context("Parsing URI Error")?;
+    Ok(())
+}
