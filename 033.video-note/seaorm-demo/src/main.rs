b80f1faf290ba598f02570fd62c6e3e1886cf585
CusterFun <me@custer.fun>
2022-09-08 00:14:41 +0800

    videonote update seaorm

--- 033.video-note/seaorm-demo/src/main.rs
+++ 033.video-note/seaorm-demo/src/main.rs
@@ -1,3 +1,8 @@
-fn main() {
-    println!("Hello, world!");
+use anyhow::Result;
+use sea_orm::Database;
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    let _connection = Database::connect("sqlite://data.sqlite?mode=rwc").await?;
+    Ok(())
 }
