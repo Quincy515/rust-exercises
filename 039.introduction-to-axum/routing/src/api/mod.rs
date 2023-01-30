4cdd6196e620bc023a14da98e949f7c90d59f11a
Custer <custertian@163.com>
2023-01-30 17:35:54 +0800

    Extracting a String

--- 039.introduction-to-axum/routing/src/api/mod.rs
+++ 039.introduction-to-axum/routing/src/api/mod.rs
@@ -1,8 +1,15 @@
 pub mod hello_world;
+pub mod mirror_body_string;
 
-use axum::{Router, routing::get};
+use axum::{
+    routing::{get, post},
+    Router,
+};
 use hello_world::hello_world;
+use mirror_body_string::mirror_body_string;
 
 pub fn create_routes() -> Router {
-    Router::new().route("/", get(hello_world))
+    Router::new()
+        .route("/", get(hello_world))
+        .route("/mirror_body_string", post(mirror_body_string))
 }
