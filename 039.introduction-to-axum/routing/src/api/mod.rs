2bad6b281178528354e6f9a97805dd4ff68c8916
Custer <custertian@163.com>
2023-01-31 18:02:14 +0800

    12. Shared Middleware Data

--- 039.introduction-to-axum/routing/src/api/mod.rs
+++ 039.introduction-to-axum/routing/src/api/mod.rs
@@ -1,4 +1,5 @@
 pub mod hello_world;
+pub mod middleware_message;
 pub mod mirror_body_json;
 pub mod mirror_body_string;
 pub mod mirror_custom_header;
@@ -8,9 +9,10 @@ pub mod query_params;
 
 use axum::{
     routing::{get, post},
-    Router,
+    Extension, Router,
 };
 use hello_world::hello_world;
+use middleware_message::middleware_message;
 use mirror_body_json::mirror_body_json;
 use mirror_body_string::mirror_body_string;
 use mirror_custom_header::mirror_custom_header;
@@ -19,8 +21,18 @@ use path_variable::{hard_coded_path, path_variable};
 use query_params::query_params;
 use tower_http::cors::{Any, CorsLayer};
 
+#[derive(Clone)]
+pub struct SharedData {
+    pub message: String,
+}
+
 pub fn create_routes() -> Router {
     let cors = CorsLayer::new().allow_methods(Any).allow_origin(Any);
+
+    let shared_data = SharedData {
+        message: "Hello from shared data".to_owned(),
+    };
+
     Router::new()
         .route("/", get(hello_world))
         .route("/mirror_body_string", post(mirror_body_string))
@@ -30,5 +42,7 @@ pub fn create_routes() -> Router {
         .route("/query_params", get(query_params))
         .route("/mirror_user_agent", get(mirror_user_agent))
         .route("/mirror_custom_header", get(mirror_custom_header))
+        .route("/middleware_message", get(middleware_message))
+        .layer(Extension(shared_data))
         .layer(cors)
 }
