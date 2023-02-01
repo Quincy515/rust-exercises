f82ed791b1a232ab56e304c2878599f62523a13f
Custer <custertian@163.com>
2023-02-01 19:35:30 +0800

    ## 20. Connecting SeaORM to the Database

--- 039.introduction-to-axum/seaorm-axum/src/main.rs
+++ 039.introduction-to-axum/seaorm-axum/src/main.rs
@@ -1,3 +1,10 @@
-fn main() {
-    println!("Hello, world!");
+use dotenvy::dotenv;
+use dotenvy_macro::dotenv;
+use seaorm_axum::run;
+
+#[tokio::main]
+async fn main() {
+    dotenv().ok();
+    let database_uri = dotenv!("DATABASE_URL");
+    run(database_uri).await;
 }
